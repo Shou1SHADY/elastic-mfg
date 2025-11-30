6a0edfa70bf399f56d1e6c75ea9c64859a8b1d3f
Shady Osama <shadyosama658@gmail.com>
2025-11-30 04:02:11 -0800

    s

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -6,7 +6,7 @@ import { Footer } from './Footer';
 export const Layout: React.FC = () => {
     return (
         <div className="min-h-screen bg-elastic-black text-zinc-100 selection:bg-elastic-accent selection:text-black flex flex-col font-sans relative overflow-hidden">
-            {/* Content */}
+            {/* Constent */}
             <div className="flex flex-col min-h-screen">
                 <Navbar />
                 <main className="flex-grow ">
